b6ebf25c5a6d62e55f58ae566347ae568e5a0a08
Alex Hoguin <ahoguin@wazo.io>
2023-11-16 17:32:26 -0500

    Fix tox tests

--- molecule/default/tests/test_with_stun_client.py
+++ molecule/default/tests/test_with_stun_client.py
@@ -12,7 +12,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     "port", ["12345", "54321"],
 )
 def test_stun(host, port):
+    matchers = ["My IP is  172", "My IP is  192"]
     cmd = host.check_output(
         "node /opt/stun/client.js {} {}".format("molecule-coturn-debian11", port)
     )
-    assert "My IP is  172" in cmd, cmd
+    assert any(match in cmd for match in matchers)
